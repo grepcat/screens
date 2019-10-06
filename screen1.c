7e93117f92ef746bfa28d6c4884a34751a38c09b
grepcat <unixnerd@protonmail.com>
2019-10-06 19:14:57 +0100

    Final Commit for now

--- screen1.c
+++ screen1.c
@@ -17,6 +17,13 @@ int main() {
   /* prints the string into the window */
   printw("%s", "Hello Again");
 
+	/* moves the cursor to the specified location */
+	move (7,15)
+
+  /* prints the string into the window */
+  printw("%s", "Hello Again for the final time");
+
+  /* refreshes the screen to match what's in memory */
   /* refreshes the screen to match what's in memory */
   refresh();
 
